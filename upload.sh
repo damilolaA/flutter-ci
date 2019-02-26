4cbe6a45edb4bdca9d07982d6a1289e492ba88b5
damilolaa <oadio@tradedepot.co>
2019-02-26 12:03:48 +0100

    update circle config file

--- upload.sh
+++ upload.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+set -eu pipefail
 
 suffix=${CIRCLE_BRANCH}_$(date -d "today" +"%Y%m%d%H%M")
+echo "$suffix";
+echo "branch ${CIRCLE_BRANCH}"
+echo "appName $app_name"
 cp ./build/app/outputs/apk/release/app-release.apk ./app_name$suffix.apk
